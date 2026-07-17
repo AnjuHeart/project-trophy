bb6f9dfb5a5e95b38210e8f2bc85760385269328
AnjuHeart <anjuheart.lt3@gmail.com>
2026-07-17 17:06:47 -0600

    preliminar library data retrieval

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,52 +1,50 @@
 import { db } from "@/lib/db";
-import LibraryView from "./LibraryView";
+import LibraryView from "./LibraryView"
 
-/*interface GameDetailsProps {
-    params: Promise<{ id: string }>;
-}*/
+export interface LibraryStats {
+    gamesCount: number;
+    awardedCount: number;
+    guidesCount: number;
+    achievementsCount: number;
+}
 
-export default async function GameDetailsPage(/*{ params }: GameDetailsProps*/) {
-    /*const { id } = await params;*/
+interface PageProps {
+    searchParams: Promise<{
+        search?: string;
+        categories?: string;
+        maxTime?: string;
+        awardType?: string;
+        minAchievements?: string;
+        genre?: string;
+        page?: string;
+    }>;
+}
 
-    /*const game = await db.game.findUnique({
-        where: {
-            slug: id
-        },
-        select: {
-            title: true,
-            logoUrl: true,
-            backgroundUrl: true,
-            difficultyRating: true,
-            totalAchievements: true,
-            blindPlaythroughHours: true,
-            minimumPlaythroughs: true,
-            timeTo100PercentPerfect: true,
-            timeTo100PercentBase: true,
-            categoryContextDescription: true,
-            genres: true,
-            completionOverview: true,
-            //Category Inner Join
-            mainCategory: {
-                select: {
-                    emoji: true,
-                    label: true,
-                    bgGradient: true,
-                    hoverGradient: true
-                }
-            },
-            //HallOfFame Outer Join
-            hallOfFameInductions: {
-                select: {
-                    reason: true,
-                    category: {
-                        select: {
-                            name: true
-                        }
-                    }
-                }
-            }
-        }
-    });*/
+const ITEMS_PER_PAGE = 12;
 
-    return <LibraryView/>;
+export default async function LibraryPage({searchParams}: PageProps) {
+    const params = await searchParams;
+
+    const currentPage = Number(params.page) || 1;
+    const searchString = params.search || "";
+    const maxTime = Number(params.maxTime) || null;
+    const minAchievements = Number(params.minAchievements) || null;
+    const awardType = params.awardType || "";
+    const genre = params.genre || "";
+    
+
+    const [gamesCount, awardedCount, guidesCount, achievementsCount] = await Promise.all([
+        db.game.count(),
+        db.hallOfFameGame.count(),
+        db.guide.count(),
+        db.achievement.count(),
+    ]);
+
+    let dbDataSet: LibraryStats = {
+        gamesCount: gamesCount,
+        awardedCount: awardedCount,
+        guidesCount: guidesCount,
+        achievementsCount: achievementsCount
+    };
+    return <LibraryView stats={dbDataSet} />;
 }
